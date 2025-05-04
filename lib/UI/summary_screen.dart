1f33b2730b7788fdb7780d3dcc2b96cb01df8ee0
Antoine CAVALLERA <antoine.cavallera@gmail.com>
2025-05-04 16:18:36 +0200

    fix summary screen layout

--- lib/UI/summary_screen.dart
+++ lib/UI/summary_screen.dart
@@ -164,7 +164,7 @@ class _SummaryScreenState extends State<SummaryScreen> with TickerProviderStateM
       margin: const EdgeInsets.only(top: 0),
       child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
